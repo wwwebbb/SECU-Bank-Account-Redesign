b0177e8dc04d082ffba2487d9093f2ff4c23c9a3
Miles Webb <milesweldo@gmail.com>
2022-09-15 19:00:43 -0400

    Add files via upload

--- index.js
+++ index.js
@@ -2,15 +2,3 @@ $("#logo").click(function () {
   window.open("https://www.ncsecu.org/");
 });
 console.log("asdas");
-
-// $(document).ready(function () {
-//   setInterval(function () {
-//     var docHeight = $(window).height();
-//     var footerHeight = $("#footer").height();
-//     var footerTop = $("#footer").position().top + footerHeight;
-//     var marginTop = docHeight - footerTop + 10;
-
-//     if (footerTop < docHeight) $("#footer").css("margin-top", marginTop + "px");
-//     else $("#footer").css("margin-top", "0px");
-//   }, 250);
-// });
